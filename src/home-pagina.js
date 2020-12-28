40e82c125c66d859bbd2b15ddcc98c098b29557b
Ffyud <5263778+Ffyud@users.noreply.github.com>
2020-12-28 18:00:52 +0100

    opsplitsen hoofpagina met lijst component

---
+++ src/home-pagina.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ItemsLijst from './items-lijst.js';
+
+class HomePagina extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          error: null,
+          isLoaded: false,
+          resultaten: []
+        };
+      }
+    
+      componentDidMount() {
+        fetch("http://127.0.0.1:5000/items")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                resultaten: result
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }
+
+    render() {
+        const { error, isLoaded, resultaten } = this.state;
+        if (error) {
+          return <div>Niet goed gegaan: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Laden...</div>;
+        } else {
+          return (
+            <div><ItemsLijst items={resultaten} /></div>
+            );
+        }
+    }
+}
+
+export default HomePagina;
