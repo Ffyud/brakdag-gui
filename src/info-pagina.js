7ab531cf475648ce3c86998b2c055d5063b069ee
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-01-14 14:34:44 +0100

    styling info-pagina

--- src/info-pagina.js
+++ src/info-pagina.js
@@ -8,15 +8,15 @@ class InfoPagina extends Component {
           Brakdag is een aggregator van nieuws. Brakdag verzamelt nieuws uit de stad Groningen van allerlei bronnen en maakt het inzichtelijk.
         </div>
         <div className='box-wide'>
-          <span><b><i className="ri-archive-line ri-2x"></i> Levend archief</b></span>
+          <span><b><i className="ri-archive-line ri-2x"></i><br/> Levend archief</b></span>
           <span> Doorzoek de vele duizenden artikelen uit Stad. Dat zoekt wat makkelijker dan alle nieuwssites bij langs te gaan.</span>
         </div>      
         <div className='box-wide'>
-          <span><b><i className="ri-database-2-line ri-2x"></i> Open Data</b></span>
+          <span><b><i className="ri-database-2-line ri-2x"></i><br/> Open Data</b></span>
           <span> Gebruik het nieuws wat Brakdag verzamelt zelf in je eigen toepassing via de API.</span>
         </div>
         <div className='box-wide'>
-          <span><b><i className="ri-open-source-line ri-2x"></i> Open Source</b></span>
+          <span><b><i className="ri-open-source-line ri-2x"></i><br/> Open Source</b></span>
           <span> Help mee verbeteren of begin een eigen Brakdag-variant. De broncode is openbaar.</span>
         </div>
       </div>
