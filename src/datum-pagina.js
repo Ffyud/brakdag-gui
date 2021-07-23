ec30ea45a13da25e3dc212b83de5d24ddca7d8bd
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-07-23 20:16:07 +0200

    toevoegen datum-kies pagina

--- src/datum-pagina.js
+++ src/datum-pagina.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import ItemsLijstPlaceholder from './placeholder/items-lijst-placeholder.js';
 import ItemsLijst from './items-lijst.js';
+import moment from 'moment';
+import DatePicker from "react-datepicker";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import nl from 'date-fns/locale/nl';
+import "react-datepicker/dist/react-datepicker.css";
+import KiesDatumPlaceholder from './placeholder/kies-datum-placeholder.js';
+
+registerLocale('nl', nl);
+setDefaultLocale('nl');
 
 class DatumPagina extends Component {
     constructor(props) {
@@ -8,21 +17,72 @@ class DatumPagina extends Component {
         this.state = {
           error: null,
           isLoaded: false,
+          resultaten: [],
+          startDate: new Date()
         };
     }
 
-    render() {
-        const { onDatumOpdracht } = this.props;
-
-        if(onDatumOpdracht === undefined) {
-            return <div><ItemsLijstPlaceholder /></div>;
+    handleDateChange = (date) => {
+      var dateFormatted = moment(date).format('DD-MM-YYYY')
+      this.setState({date: dateFormatted, startDate: date});
+      console.log("de datepicker heeft een date " + dateFormatted);
+  
+      fetch(process.env.REACT_APP_BRAKDAGFLASK+"/items/datum/" + dateFormatted)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            resultaten: result
+          });
+          // this.props.dateOpdracht(result);
+          console.log(" resultaten!" + result.length)
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
-        else if(onDatumOpdracht.length ===  0) {
-            return <div><ItemsLijstPlaceholder /></div>;
+      )
+    };
+
+    render() {
+      if(this.state.resultaten.length ===  0) {
+            return <div>
+                  <div className="pagina-header-date-pick">
+                    <DatePicker 
+                    peekNextMonth
+                    showMonthDropdown
+                    dropdownMode="select"
+                    minDate={new Date("01-04-2016")} 
+                    placeholderText="Kies een datum" 
+                    selected={this.state.startDate} 
+                    locale="nl" 
+                    dateFormat="EEEE dd MMMM yyyy" 
+                    onChange={this.handleDateChange} />
+                  </div>
+              <ItemsLijstPlaceholder />
+            <KiesDatumPlaceholder />
+            </div>;
         }
         else {
             return (
-                <div><ItemsLijst items={onDatumOpdracht} /></div>
+                <div>
+                  <div className="pagina-header-date-pick">
+                    <DatePicker 
+                    peekNextMonth
+                    showMonthDropdown
+                    dropdownMode="select"
+                    minDate={new Date("01-04-2016")} 
+                    placeholderText="Kies een datum" 
+                    selected={this.state.startDate} 
+                    locale="nl" 
+                    dateFormat="EEEE dd MMMM yyyy" 
+                    onChange={this.handleDateChange} />
+                  </div>
+                  <ItemsLijst items={this.state.resultaten} />
+                </div>
             );
         }
     }
---
+++ src/placeholder/kies-datum-placeholder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class KiesDatumPlaceholder extends Component {
+
+  render() {
+    return (
+      <div className='niet-gezocht-placeholder'>
+        Kies een datum om het nieuws van die dag te zien.
+      </div>
+    );
+  }
+}
+
+export default KiesDatumPlaceholder;
