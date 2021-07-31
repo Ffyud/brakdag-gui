de6860d96bd5aabfed120cb38b5f511c28ce3254
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-07-31 22:42:42 +0200

    uitgelicht scrollbaar

--- src/items-uitgelicht-lijst.js
+++ src/items-uitgelicht-lijst.js
@@ -5,30 +5,15 @@ class ItemsUitgelichtLijst extends Component {
         super(props)
         this.state = {
             beginMap: 0,
-            eindeMap: 3
+            eindeMap: 15
         };
 
-    }
-
-    gaVerder = () => {
-        if (this.state.eindeMap === 10) {
-            this.setState({
-                beginMap: 0,
-                eindeMap: 3
-            });
-        }
-        else {
-            this.setState({
-                beginMap: this.state.beginMap + 1,
-                eindeMap: this.state.eindeMap + 1
-            });
-        }
     }
 
     render() {
         const { items } = this.props;
         return (
-            <div id='wrap'>
+            <div id='wrap' className="wrap-overflow">
                 <ul className='lijst-flex'>
                     {items.slice(this.state.beginMap, this.state.eindeMap).map(item => (
                         <li key={item['id']}>
@@ -44,7 +29,6 @@ class ItemsUitgelichtLijst extends Component {
                             </div>
                         </li>
                     ))}
-                    <li className='lijst-next'><div onClick={this.gaVerder}><i className="ri-arrow-right-line"></i></div></li>
                 </ul>
             </div>
         );
